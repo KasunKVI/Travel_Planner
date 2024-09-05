330a985e930f2b915a0942154732e5f2bda8e93f
Kasun Kavinda <kkhettiarachchi10@gmail.com>
2024-09-05 16:14:04 +0530

    implement the main class

---
+++ Travel_Planner/src/main/java/software/kasunkavinda/Travel_Planner/TravelPlannerApplication.java
@@ -0,0 +1,19 @@
+package software.kasunkavinda.Travel_Planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class TravelPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPlannerApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
