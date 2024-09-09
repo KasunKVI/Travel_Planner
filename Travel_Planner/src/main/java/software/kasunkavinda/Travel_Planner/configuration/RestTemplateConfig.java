1a3e476861dfce61fc6c206b48b3b5e59fce0567
Kasun Kavinda <kkhettiarachchi10@gmail.com>
2024-09-09 10:13:58 +0530

    implement the Rest template bean
    Signed-off-by: Kasun Kavinda <kkhettiarachchi10@gmail.com>

---
+++ Travel_Planner/src/main/java/software/kasunkavinda/Travel_Planner/configuration/RestTemplateConfig.java
@@ -0,0 +1,15 @@
+package software.kasunkavinda.Travel_Planner.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
